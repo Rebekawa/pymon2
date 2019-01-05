d78678a23e781d6b232aeeec7536c7178e5948cc
Rebecca Kawa <reb.kawa@gmail.com>
2019-01-05 20:46:06 +0200

    first commit

---
+++ frontend/src/components/backbutton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Backbutton extends React.Component {
+
+    render() {
+        return (
+        <a className="backButton" href="/games" >
+            <i class="fas fa-long-arrow-alt-left"></i>
+            Go Back
+        </a>
+        )
+    }
+}
